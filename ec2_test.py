657b37d40418a02c8df6cf46536d8290705deb9f
yueranyuan <yueranyuan@gmail.com>
2014-12-10 17:43:39 -0500

    some basic demonstration of the capabilities to launch ec2 instances and run basic stuff on it

--- ec2_test.py
+++ ec2_test.py
@@ -1,10 +1,10 @@
 import boto.ec2
-import time
-import sys
-from boto.manage.cmdshell import sshclient_from_instance as sshclient
+from fabric.api import run, execute, env
+from fabric.network import disconnect_all
 
 def connect(region="us-east-1"):
     return boto.ec2.connect_to_region("us-east-1")
+
 
 def stop_all(conn):
     reserves = conn.get_all_reservations()
@@ -14,6 +14,7 @@ def stop_all(conn):
             print 'stopping: ', to_stop
             conn.stop_instances(instance_ids=to_stop)
 
+
 def terminate_all(conn):
     reserves = conn.get_all_reservations()
     for r in reserves:
@@ -22,31 +23,40 @@ def terminate_all(conn):
             print 'terminating: ', to_stop
             conn.terminate_instances(instance_ids=to_stop)
 
+
+def reserve(conn):
+    print 'launching instance'
+    reserve = conn.run_instances('ami-b66ed3de', key_name='cmu-east-key1',
+                                 instance_type='t2.micro', security_groups=['Aaron-CMU-East'])
+    print 'launched instance ' + reserve.instances[0].id
+    return reserve
+
+
+def ssh_connect(instance):
+    instance.update()
+    if instance.state != 'running':
+        raise Exception("instance {0} not running yet".format(instance.id))
+    print 'connecting to instance {0}'.format(instance.id)
+    return instance.ip_address
+
 #stop_all(connect())
-terminate_all(connect())
+#terminate_all(connect())
+#conn = connect()
+#res = reserve(conn)
+conn = connect()
+res = conn.get_all_reservations()[0]
+addr = ssh_connect(res.instances[0])
+env.key_filename = "cmu-east-key1.pem"
 
-conn = boto.ec2.connect_to_region("us-east-1")
 
-print 'launching instance'
-reserve = conn.run_instances('ami-b66ed3de', key_name='cmu-east-key1',
-                    instance_type='t2.micro', security_groups=['Aaron-CMU-East'])
-print 'launched instance'
+def do_work(cmd):
+    return run(cmd)
 
-def run_stuff(inst):
-    ssh_client = sshclient(inst,
-                            ssh_key_file='cmu-east-key1.pem',
-                            user_name='root')
-    status, stdout, stderr = ssh_client.run('pwd')
-    print status, stdout, stderr
-    conn.stop_instances(instance_ids=[inst.id])
 
-for i in range(20):
-    inst = reserve.instances[0]
-    inst.update()
-    print inst.state
-    if inst.state == "running":
-        run_stuff(inst)
-        break
-    time.sleep(15)
+def deploy(cmd, addr):
+    host_list = ['ec2-user@{0}'.format(addr)]
+    results = execute(do_work, cmd, hosts=host_list)
+    print results
 
-#conn.stop_instances(reserve.instances)
+deploy('pwd', addr)
+disconnect_all()

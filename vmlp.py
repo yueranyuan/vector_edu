5fc4bcd819e50d2c9d3bc787477cc8249d0959f4
Yueran Yuan <yueranyuan@gmail.com>
2014-12-12 11:40:47 -0500

    * fixing vmlp.py by linter * removing old file

--- vmlp.py
+++ vmlp.py
@@ -1,14 +1,8 @@
-import os
-import sys
-import time
-
 import numpy
-
 import theano
 import theano.tensor as T
-
-from logistic_sgd import LogisticRegression
 from mlp import MLP
+
 
 class VectorLayer(object):
     def __init__(self, rng, input, full_input, n_skills=4600, vector_length=30):
@@ -19,26 +13,22 @@ class VectorLayer(object):
         skill_i = T.cast(input, 'int32')
         self.output = self.skills[skill_i[:, 0]]
         self.m = theano.shared(reindex(full_input.get_value(borrow=True),
-                                        self.skills.get_value(borrow=True)),
-                            borrow=True)
+                                       self.skills.get_value(borrow=True)),
+                               borrow=True)
 
     def get_updates(self, cost, index, batch_size, learning_rate):
         gx = T.grad(cost, self.output)
-        gskills = T.dot(self.m[:, index *  batch_size: (index + 1) * batch_size], gx)
+        gskills = T.dot(self.m[:, index * batch_size: (index + 1) * batch_size], gx)
         return [(self.skills, self.skills - learning_rate * gskills)]
 
-    def debug(self, cost, index):
-        gx = T.grad(cost, self.output)
-        asdf = self.m[:, index * 3: (index + 1) * 3]
-        return asdf
 
 class VMLP(object):
     def __init__(self, rng, input, vector_length, n_skills, n_hidden, n_out, full_input):
         self.vectors = VectorLayer(rng=rng,
-            input=input,
-            full_input=full_input,
-            n_skills=n_skills,
-            vector_length=30)
+                                   input=input,
+                                   full_input=full_input,
+                                   n_skills=n_skills,
+                                   vector_length=30)
 
         self.MLP = MLP(
             rng=rng,
@@ -56,6 +46,7 @@ class VMLP(object):
 
         self.params = self.MLP.params
         self.get_updates = self.vectors.get_updates
+
 
 def reindex(skills, skillVecs, y=None):
     y = y or numpy.zeros([len(skillVecs), len(skills)])

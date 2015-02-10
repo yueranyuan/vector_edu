5c50d1a4977dc9ed32a65df81ee7a70e5384afdb
Jeffrey Zhang <caretcaret@gmail.com>
2015-02-09 22:31:16 -0500

    Preparing some raw data.
    I am probably unaware of something that I am doing wrong, halp
    -> a bunch of frequency bins are 0s?
    -> normalized table has NaNs but input table does not
    
    To generate a pickle from raw data,
    python emotiv_driver.py convert_raw <directory> <destination>
    You may want to do this on a smaller directory for testing, the full
    data takes a few minutes to load.
    
    python emotiv_driver.py run_raw -f <pickle> to run the base network on
    the raw data. The raw data version of prepare_data is segment_raw_data,
    which:
    1. extracts all consecutive sections of 10 seconds that have the same
    label/condition
    2. does fourier transform in 1s windows, offset by 0.5s
    3. concats all those together into a 1120 length vector as the "eeg
    vector"
    
    This runs but doesn't train out of the box (50.10% validation accuracy).
    Copy the base model to something like raw_base.py if you want to
    experiment with different architectures. It may be worthwhile to reshape
    the 1120 length vector into 20 (# windows) * 4 (# bins) * 14 (# num
    channels) to do some recurrent thing over the window sequence.

--- emotiv_driver.py
+++ emotiv_driver.py
@@ -5,13 +5,14 @@ into a Dataset object stored in the output file.
 
 Usage:
     emotiv_driver.py run [options]
+    emotiv_driver.py run_raw [options]
     emotiv_driver.py convert_raw <directory> <output>
 
 Options:
     -p <param_set>, --param_set=<param_set>
         The name of the parameter set to use [default: emotiv_wide_search].
     -f <file>, --file=<file>
-        The data file to use.
+        The data file to use [default: raw_data/all_siegle.txt].
     -o <file>, --out=<file>
         The name for the log file to be generated.
     -q, --quiet
@@ -22,36 +23,36 @@ Options:
 
 from __future__ import print_function, division
 
-from datetime import datetime
 import os
-import traceback
-import glob
-import cPickle as pickle
 import warnings
 warnings.filterwarnings("ignore", category=DeprecationWarning)
 
 from docopt import docopt
 
 from learntools.libs.logger import gen_log_name, log_me, set_log_file
-from learntools.emotiv.data import prepare_data
+from learntools.emotiv.data import prepare_data, convert_raw_data, segment_raw_data
 from learntools.data import cv_split, Dataset
-from learntools.data.dataset import LISTEN_TIME_FORMAT
 import learntools.deploy.config as config
-from learntools.libs.utils import loadmat
 
 import release_lock
 release_lock.release()  # TODO: use theano config instead. We have to figure out
 # what they did with the config.compile.timeout variable because that's actually
 # what we need
 
+class ModelType(object):
+    BASE = 0
+    RAW_BASE = 1
 
 @log_me()
-def run(task_num=0, model_type=0, **kwargs):
-    if model_type == 0:
+def run(task_num=0, model_type=ModelType.BASE, **kwargs):
+    if model_type == ModelType.BASE:
         from learntools.emotiv.base import BaseEmotiv as SelectedModel
+        dataset = prepare_data(**kwargs)
+    elif model_type == ModelType.RAW_BASE:
+        from learntools.emotiv.base import BaseEmotiv as SelectedModel
+        dataset = segment_raw_data(**kwargs)
     else:
         raise Exception("model type is not valid")
-    dataset = prepare_data(**kwargs)
     train_idx, valid_idx = cv_split(dataset, percent=0.1, fold_index=task_num)
     prepared_data = (dataset, train_idx, valid_idx)
 
@@ -59,45 +60,7 @@ def run(task_num=0, model_type=0, **kwargs):
     model.train_full(**kwargs)
 
 
-def convert_raw(directory, output):
-    raw_files = glob.glob(os.path.join(directory, '*.mat'))
-
-    subjects = {}
-    headers = [
-        ('subject', Dataset.STR),
-        ('eeg_sequence', Dataset.SEQFLOAT),
-        ('condition', Dataset.SEQINT), # TODO is this the column type we really want?
-        ('time', Dataset.TIME),
-    ]
-
-    n_rows = len(raw_files)
-    ds = Dataset(headers, n_rows)
-
-    for i, raw_filename in enumerate(raw_files):
-        print(raw_filename)
-        try:
-            raw_file = loadmat(raw_filename)
-            filename, extension = os.path.splitext(os.path.basename(raw_filename))
-            p = raw_file['p']
-            eeg_sequence = p['EEG']
-            condition = p['OtherData'][2, :]
-            dt = datetime(*tuple(p['hdr']['orig']['T0']))
-            timestr = dt.strftime(LISTEN_TIME_FORMAT)
-
-            ds[i] = (filename, eeg_sequence, condition, timestr)
-        except Exception as e:
-            traceback.print_exc()
-            return # fail hard
-
-    print(len(raw_files), "files loaded")
-    
-    with open(output, 'wb') as f:
-        pickle.dump(ds.to_pickle(), f, protocol=pickle.HIGHEST_PROTOCOL)
-
-
 if __name__ == '__main__':
-    default_dataset = "raw_data/all_siegle.txt"
-
     args = docopt(__doc__)
 
     params = config.get_config(args['--param_set'])
@@ -109,14 +72,14 @@ if __name__ == '__main__':
 
     if args['--file']:
         params['dataset_name'] = args['--file']
-    elif 'dataset_name' not in params:
-        params['dataset_name'] = default_dataset
 
     params['conds'] = ['EyesClosed', 'EyesOpen']
 
     if args['run']:
-        run(0, **params)
+        run(task_num=0, model_type=ModelType.BASE, **params)
+    elif args['run_raw']:
+        run(task_num=0, model_type=ModelType.RAW_BASE, **params)
     elif args['convert_raw']:
-        convert_raw(args['<directory>'], args['<output>'])
+        convert_raw_data(args['<directory>'], args['<output>'])
     
     print("Finished")
--- learntools/emotiv/data.py
+++ learntools/emotiv/data.py
@@ -1,8 +1,58 @@
+from __future__ import print_function, division
+
+from datetime import datetime
 import itertools
-from itertools import izip
+import cPickle as pickle
+import glob
+import numpy as np
+import os
+import traceback
 
 from learntools.data import Dataset
-from learntools.libs.utils import normalize_table
+from learntools.data.dataset import LISTEN_TIME_FORMAT
+from learntools.libs.utils import normalize_table, loadmat
+from learntools.libs.eeg import signal_to_freq_bins
+
+# headers used in the raw data taken directly from matlab
+RAW_HEADERS = [
+    ('subject', Dataset.STR),
+    ('eeg_sequence', Dataset.SEQFLOAT),
+    ('condition', Dataset.SEQINT), # TODO is this the column type we really want?
+    ('time', Dataset.TIME),
+]
+
+# headers used in the raw data that has been segmented
+SEGMENTED_HEADERS = [
+    ('subject', Dataset.ENUM), # subject id
+    ('source', Dataset.STR), # denotes the file and eeg index where the segment was taken from
+    ('eeg', Dataset.MATFLOAT), # fixed duration due to truncation for uniformity
+    ('condition', Dataset.ENUM), # only one label characterizes the sequence
+]
+
+
+ACTIVITY_CONDITIONS = {
+    'EyesOpen': 1,
+    'EyesClosed': 2,
+    'rest': 3,
+    'NegativeHighArousalPictures': 4,
+    'NeutralLowArousalPictures': 5,
+    'PositiveHighArousalPictures': 6,
+    'NegativeLowArousalPictures': 7,
+    'PositiveLowArousalPictures': 8,
+    'EroticHighArousalPictures': 9,
+    'CountBackwards': 10,
+    'Ruminate': 11,
+    'DrawStickFigure': 12,
+}
+
+META_CONDITIONS = {
+    'BEGIN': 132,
+    'END': 136,
+    'INNER': 0,
+}
+
+CONDITIONS = dict(ACTIVITY_CONDITIONS.items() + META_CONDITIONS.items())
+CONDITIONS_STR = dict((v, k) for k, v in CONDITIONS.items())
 
 
 def prepare_data(dataset_name, conds=None, **kwargs):
@@ -27,7 +77,7 @@ def prepare_data(dataset_name, conds=None, **kwargs):
     data.rename_column('fname', 'group')
     data.rename_column('Condition', 'condition')
     data.set_column('eeg', Dataset.MATFLOAT)
-    for i, eeg in enumerate(izip(*[data[h] for (h, _) in eeg_headers])):
+    for i, eeg in enumerate(itertools.izip(*[data[h] for (h, _) in eeg_headers])):
         data.get_column('eeg')[i] = eeg
     data.get_column('eeg').data = normalize_table(data['eeg'])
 
@@ -43,3 +93,151 @@ def prepare_data(dataset_name, conds=None, **kwargs):
         for i, c in enumerate(cond_data):
             data.get_column('condition')[i] = c
     return data
+
+
+def convert_raw_data(directory, output):
+    raw_files = glob.glob(os.path.join(directory, '*.mat'))
+
+    subjects = {}
+
+    n_rows = len(raw_files)
+    ds = Dataset(RAW_HEADERS, n_rows)
+
+    for i, raw_filename in enumerate(raw_files):
+        print(raw_filename)
+        try:
+            raw_file = loadmat(raw_filename)
+            filename, extension = os.path.splitext(os.path.basename(raw_filename))
+            p = raw_file['p']
+            eeg_sequence = p['EEG']
+            condition = p['OtherData'][2, :]
+            dt = datetime(*tuple(p['hdr']['orig']['T0']))
+            timestr = dt.strftime(LISTEN_TIME_FORMAT)
+
+            ds[i] = (filename, eeg_sequence, condition, timestr)
+        except Exception as e:
+            traceback.print_exc()
+            raise e
+
+    print(len(raw_files), "files loaded")
+
+    with open(output, 'wb') as f:
+        pickle.dump(ds.to_pickle(), f, protocol=pickle.HIGHEST_PROTOCOL)
+
+
+def _segment_gen(segment_idx, segment_cond):
+    """Generator for segment (begin, end, label). Coalesces cascading segments with the same label.
+    segment_idx: numpy array for the indices for the beginning of each label
+    segment_cond: numpy array the condition started by index"""
+
+    length = len(segment_idx)
+    if length == 0:
+        raise StopIteration
+
+    # iterate through segments, coalescing when possible
+    i = 0
+    while i < length - 2:
+        # find the segment end
+        j = i + 1
+        cond = segment_cond[i]
+        while j < length - 1 and segment_cond[j] == cond:
+            j += 1
+
+        yield segment_idx[i], segment_idx[j], cond
+        i = j
+
+
+def segment_raw_data(dataset_name, conds=None, duration=10, sample_rate=128, **kwargs):
+    """Loads raw siegle data from a pickled Dataset and extracts sequences of
+    eeg vectors which have a single known label.
+
+    Args:
+        ds: the raw Dataset
+        duration: duration the segment should be, in seconds
+        sample_rate: the number of eeg vectors per second
+    Returns:
+        Dataset
+    """
+    with open(dataset_name, 'rb') as f:
+        ds = Dataset.from_pickle(pickle.load(f))
+
+    subjects = []
+    segments = []
+
+    conds_values = [ACTIVITY_CONDITIONS[k] for k in conds] if conds is not None else ACTIVITY_CONDITIONS.values()
+
+    # extract segments of relevant activity from the eeg vector sequence
+    for i in xrange(len(ds)):
+        subject, eeg_seq, condition_seq, rec_time = ds[i]
+
+        # find the nonzero elements of condition_seq, which are the actual labels
+        segment_idx = np.nonzero(condition_seq)[0]
+        segment_cond = condition_seq[segment_idx]
+
+        # The beginning/ending of a recording is denoted by 132 and 136, respectively;
+        # only use the sequence if they exist
+        if CONDITIONS['BEGIN'] not in segment_cond and CONDITIONS['END'] not in segment_cond:
+            print("begin and end not in eeg sequence", subject)
+            continue
+
+        # The beginning of a segment is denoted by that label; inside the segment,
+        # the label is 0; the end is denoted by another label.
+        for segment_begin, segment_end, label in _segment_gen(segment_idx, segment_cond):
+            source = "{}@{}".format(subject, segment_begin)
+
+            # don't use segment if the label is unknown
+            if label not in ACTIVITY_CONDITIONS.values():
+                continue
+
+            # filter out unwanted labels
+            if label not in conds_values:
+                continue
+
+            # if segment is too short, don't use
+            segment_samples = segment_end - segment_begin
+
+            if segment_samples < duration * sample_rate:
+                print(source, "with length", segment_samples, "too small")
+                continue
+            else:
+                # truncate sample to duration seconds
+                segment_end = min(segment_end, segment_begin + duration * sample_rate)
+                # shape should be (duration * sample_rate) by eeg vector length
+                eeg_segment = eeg_seq[segment_begin:segment_end, :]
+
+                # get subject id or generate one
+                if subject not in subjects:
+                    subjects.append(subject)
+                    subject_id = subjects.index(subject)
+                else:
+                    subject_id = subjects.index(subject)
+
+                # Fourier transform on eeg
+                # Window size of 1 s, overlap by 0.5 s
+                eeg_freqs = []
+
+                for i in (x * 0.5 for x in xrange(duration * 2)):
+                    # window is half second duration (in samples) by eeg vector length
+                    window = eeg_segment[int(i * sample_rate/2) : int((i + 1) * sample_rate/2)]
+                    # there are len(cutoffs)-1 bins, window_freq is a list of will have a frequency vector of num channels
+                    window_freq = signal_to_freq_bins(window, cutoffs=[0.5, 4.0, 7.0, 12.0, 30.0], sampling_rate=128.0)
+
+                    eeg_freqs.append(np.concatenate(window_freq))
+
+                # (num windows * num bins) * num channels
+                eeg_freqs = np.concatenate(eeg_freqs)
+
+                # Flatten into a vector
+                eeg_freqs_flattened = np.ravel(eeg_freqs)
+
+                segments.append((subject_id, source, eeg_freqs_flattened, label))
+
+    # add all segments to the new dataset
+    new_ds = Dataset(SEGMENTED_HEADERS, len(segments))
+    for i, seg_data in enumerate(segments):
+        new_ds[i] = seg_data
+
+    # TODO normalization should be shared across some columns
+    new_ds.get_column('eeg').data = normalize_table(new_ds['eeg'])
+
+    return new_ds
